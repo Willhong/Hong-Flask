2756964169c20ed102c3961b3f55904ed6af2fc3
Willhong <hbilly2002@gmail.com>
2021-09-02 16:41:22 +0900

    Third Commit

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
-from flask import Flask
-from flask import render_template
-
+from datetime import datetime
+ 
+from flask import Flask, render_template
+ 
 app = Flask(__name__)
-
-@app.route('/') 
+ 
+posts = [
+    {
+        'author': {
+            'username': 'test-user'
+        },
+        'title': '첫 번째 포스트',
+        'content': '첫 번째 포스트 내용입니다.',
+        'date_posted': datetime.strptime('2018-08-01', '%Y-%m-%d')
+    },
+    {
+        'author': {
+            'username': 'test-user'
+        },
+        'title': '두 번째 포스트',
+        'content': '두 번째 포스트 내용입니다.',
+        'date_posted': datetime.strptime('2018-08-03', '%Y-%m-%d')
+    },
+]
+ 
+@app.route('/')
+@app.route('/index')
 def index():
-
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
+ 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+  return render_template('about.html', title='About')
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=False)
+
+
+#https://opentutorials.org/module/3669/22003
+#https://wikidocs.net/143921
