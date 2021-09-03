0fd19054cb45a0be9a413b8a520231e4351833f1
Willhong <hbilly2002@gmail.com>
2021-09-03 15:54:50 +0900

    moooooooya ho

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from datetime import datetime
 from flask import Flask, render_template
 from flask import request
 import json
+import threading
+import youtube_dl
+from youtube_dl.utils import args_to_str
 
-import win32ui
+#import win32ui
 
 app = Flask(__name__)
  
@@ -26,7 +29,33 @@ posts = [
         'date_posted': datetime.strptime('2018-08-03', '%Y-%m-%d')
     },
 ]
- 
+
+finish_log=""
+class MyLogger(object):
+    def debug(self, msg):
+        print(msg)
+
+    def warning(self, msg):
+        print(msg)
+
+    def error(self, msg):
+        print(msg)
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        finish_log="Done downloading, now converting ..."
+
+def download_yt(url):
+	ydl_opts = {
+        'logger': MyLogger(),
+        'progress_hooks': [my_hook],
+    }
+	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+	 ydl.download([url])
+     
+     
+
+
 @app.route('/')
 
 @app.route('/index')
@@ -35,9 +64,10 @@ def index():
  
 @app.route('/about')
 def about():
-    o = win32ui.CreateFileDialog( 1, ".txt", "default.txt", 0, "Text Files (*.txt)|*.txt|All Files (*.*)|*.*|")
+    """o = win32ui.CreateFileDialog( 1, ".txt", "default.txt", 0, "Text Files (*.txt)|*.txt|All Files (*.*)|*.*|")
     o.DoModal()
     print (o.GetPathName())
+    """
     
     return render_template('about.html', title='About')
 
@@ -48,9 +78,21 @@ def api():
 
     d = {"text": "Hello {}".format(to)}
     return json.dumps(d)
-    
+@app.route('/youtube',methods=['POST','GET'])
+def youtube():
+    if request.method == 'POST':
+        value = request.form['test']
+        """ydl =threading.Thread(target=download_yt(value))
+        ydl.start()"""
+        download_yt(value)
+        return render_template('youtube.html',test=finish_log)
+    elif request.method=='GET':
+        return render_template('youtube.html')
+
+
+
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=False)
 
 
 
